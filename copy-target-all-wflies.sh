e537b898359ebed64e8d65a7ecde459d5d918968
Miloslav Žežulka <mzezulka@eduroam47-74.fi.muni.cz>
2019-09-25 07:28:34 +0200

    Add tracing dependency and update script.

--- copy-target-all-wflies.sh
+++ copy-target-all-wflies.sh
@@ -1,5 +1,3 @@
-set -eux
-
 for WILDFLY_DIR in $WILDFLY_HOME ~/Software/wfly_copies/*
   do printf "copying target jar to ${WILDFLY_DIR}\n"; cp target/jboss-ejb-client-*.Final-SNAPSHOT.jar ${WILDFLY_DIR}/modules/system/layers/base/org/jboss/ejb-client/main/jboss-ejb-client.jar
 done
